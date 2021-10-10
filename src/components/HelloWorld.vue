55ae86ca1796010b0ec7dafde81641f537b32ee1
Muhammad Numan <numanrazzaq24@gmail.com>
2021-10-10 20:41:07 +0500

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,194 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <div v-if="user">
-      <div v-for="use in user" :key="use.id">
-        <h1>{{ use.Name }}</h1>
-        <p>{{ use.title }}</p>
-      </div>
-        <!-- <div>
-          <label for="UName">Enter Name</label><br />
-          <input type="text" id="UName" v-model="User.Name" /><br /><br />
-          <label for="title">Enter Title</label><br />
-          <input type="text" id="title" v-model="User.title" /><br /><br />
-          <button @click="Save">Add User</button>
-        </div> -->
-    </div>
-    <div v-else><p>Loading ...</p></div>
+  <div v-if="User">
+    <v-card>
+      <v-card-title>Users</v-card-title>
+      <v-row>
+        <v-col cols="4">
+          <v-text-field
+            label="Search"
+            v-model="user"
+            single-line
+            prepend-inner-icon="mdi-magnify"
+          ></v-text-field>
+        </v-col>
+        <v-spacer></v-spacer>
+        <div class="text-center">
+          <v-dialog v-model="dialog" width="700">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                plain
+                text
+                rounded
+                class="primary mr-4"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-account-plus</v-icon>
+                <span>Add User</span>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                Enter New User Details
+              </v-card-title>
+              <v-card-text>
+                <v-text-field
+                  label="UserId"
+                  v-model="todos.userId"
+                ></v-text-field
+                ><v-text-field label="ID" v-model="todos.id"></v-text-field
+                ><v-text-field
+                  label="Title"
+                  v-model="todos.title"
+                ></v-text-field
+                ><v-text-field
+                  label="Bio"
+                  v-model="todos.completed"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="Save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </v-row>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="User"
+          multi-sort
+          :search="user"
+          :items-per-page="6"
+        >
+          <template v-slot:item.menu="">
+            <v-menu transition="scale-transition" origin="center center">
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon v-bind="attrs" v-on="on">mdi-dots-vertical</v-icon>
+              </template>
+              <v-list>
+                <v-list-item>
+                  <v-dialog v-model="dialog" width="700">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        plain
+                        text
+                        rounded
+                        class="primary mr-4"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <span>Edit</span>
+                      </v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title class="text-h5 grey lighten-2">
+                        Change User Details
+                      </v-card-title>
+                      <v-card-text>
+                        <v-text-field
+                          label="UserId"
+                          v-model="todos.userId"
+                        ></v-text-field
+                        ><v-text-field
+                          label="ID"
+                          v-model="todos.id"
+                        ></v-text-field
+                        ><v-text-field
+                          label="Title"
+                          v-model="todos.title"
+                        ></v-text-field
+                        ><v-text-field
+                          label="Bio"
+                          v-model="todos.completed"
+                        ></v-text-field>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn @click="Change">Save Changes</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-list-item>
+                <v-list-item>
+                  <v-btn plain text rounded class="primary mr-4">Delete </v-btn>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
-
 <script>
-import {mapState,mapActions} from 'vuex'
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-data() {
-  return {
-  // User:{},
-  }
-},
-computed: {
-    // user() {
-    //   return store.state.user;
-    // },
-    ...mapState(['user'])
+  name: "Hello-World",
+  data() {
+    return {
+      user: "",
+      dialog: false,
+      User: [],
+      todos: { userId: 0, id: 0, title: "", completed: "" },
+      headers: [
+        { text: "UserId", value: "userId" },
+        { text: "ID", value: "id" },
+        { text: "Title", value: "title" },
+        { text: "Bio", value: "completed" },
+        { text: "", value: "menu", sortable: false },
+      ],
+    };
   },
   methods: {
-    ...mapActions(["GetUserAct"]),
+    loadData() {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => response.json())
+        .then((json) => {
+          this.User = json;
+        });
+    },
     Save() {
-      // this.User.id++;
-      // this.GetUserAct(this.User);
-    }
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        body: JSON.stringify(this.todos),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => this.User.push(json));
+      this.dialog = false;
+    },
+    delete() {
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "DELETE",
+        body: JSON.stringify(this.todos),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => (this.User = json));
+    },
+    Change() {
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "PUT",
+        body: JSON.stringify(this.todos),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => (this.User = json));
+      this.dialog = false;
+    },
+  },
+  mounted() {
+    this.loadData();
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,15 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to My Vue App"/>
-
-    <div v-for="hero in heroes" :key=hero.fName>
-      <p>{{hero.fName}}</p>
-      <router-link :to="{name: 'Child', params:{id: hero.fName} }">Go to child</router-link>
-    </div>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
-   data() {
-     return {
-       heroes:[
-        {fName:'Sufyan',lName:'Khan'},
-        {fName:'Numan',lName:'Khan'},
-        {fName:'Abdulllah',lName:'Khan'},
-       ]
-     }
-   },
-  
-}
+  export default {
+    name: 'Home',
 
+    components: {
+      HelloWorld,
+    },
+  }
 </script>
